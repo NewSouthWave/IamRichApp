6106c71397270ec0a80cd05f959233a62ab23ccc
NewSouthWave <tigers0110@naver.com>
2025-01-19 17:44:04 +0900

    Initial Commit

---
+++ IamRichApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IamRichApp
+//
+//  Created by Nam on 1/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
